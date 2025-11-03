474cb0e300425e6e795bace3de5db974c199b557
rahatiftekharbd <rahatiftekhar3@gmail.com>
2025-11-03 23:52:40 +0600

    Create 4.dart

---
+++ 4.dart
@@ -0,0 +1,19 @@
+class Animal {
+  int id;
+  String name;
+  String color;
+  Animal(this.id, this.name, this.color);
+}
+
+class Cat extends Animal {
+  String sound;
+  Cat(int id, String name, String color, this.sound) : super(id, name, color);
+  void display() {
+    print('ID: $id, Name: $name, Color: $color, Sound: $sound');
+  }
+}
+
+void main() {
+  var c = Cat(1, 'Kitty', 'White', 'Meow');
+  c.display();
+}
